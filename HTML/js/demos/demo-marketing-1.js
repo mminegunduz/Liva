22141f7023a6ce5978ec33598e724112d0488db8
Mine <minegndz57@gmail.com>
2025-08-07 16:12:33 +0300

    new updates added.

--- HTML/js/demos/demo-marketing-1.js
+++ HTML/js/demos/demo-marketing-1.js
@@ -35,15 +35,15 @@ Theme Version:  12.1.0
     /*
 	Image Change
 	*/
-    gsap.defaults({ overwrite: 'auto'});
-
-    // Set up our scroll trigger
-    const ST = ScrollTrigger.create({
-		trigger: ".gsap-content-container",
+  gsap.utils.toArray(".gsap-content-container").forEach(container => {
+	ScrollTrigger.create({
+		trigger: container,
 		start: "top top",
 		end: "bottom bottom",
-		onUpdate: getCurrentSection
+		onUpdate: () => getCurrentSection(container)
 	});
+});
+
 
     const contentMarkers = gsap.utils.toArray(".gsap-content-marker");
 
@@ -72,31 +72,24 @@ Theme Version:  12.1.0
     // Handle the updated position
     let lastContent;
 
-    function getCurrentSection() {
-		let newContent;
-		const currScroll = scrollY;
+   function getCurrentSection(container) {
+	const currScroll = scrollY;
+	let newContent;
+	const markers = container.querySelectorAll('.gsap-content-marker');
 
-		// Find the current section
-		contentMarkers.forEach(({offsetTop, content}) => {
-			if (currScroll > (offsetTop - 100)) {
-				newContent = content;
-			}
-		});
-	
-		// If the current section is different than that last, animate in
-		if (newContent &&
-			(lastContent == null ||
-				!newContent.isSameNode(lastContent))) {
-			// Fade out last section
-			if (lastContent) {
-				lastContent.leave();
-			}
-	
-			// Animate in new section
-			newContent.enter();
-	
-			lastContent = newContent;
+	markers.forEach(marker => {
+		const { offsetTop } = marker;
+		const content = document.querySelector(`#${marker.dataset.markerContent}`);
+		if (currScroll > (offsetTop - 100)) {
+			newContent = content;
 		}
-	
+	});
+
+	if (newContent && (!lastContent || !newContent.isSameNode(lastContent))) {
+		if (lastContent) lastContent.leave();
+		newContent.enter();
+		lastContent = newContent;
 	}
+}
+
 })).apply( this, [ jQuery ]);
